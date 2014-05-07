aa809984723fa6c88ee4b65cc6b84a50a20cfd68
Filip Hrisafov <filip.hrisafov@gmail.com>
2014-05-07 22:32:34 +0200

    selection methods changed

--- src/ch/epfl/bigdata/ts/ga/selection/RouletteWheelSelection.java
+++ src/ch/epfl/bigdata/ts/ga/selection/RouletteWheelSelection.java
@@ -9,6 +9,8 @@ public class RouletteWheelSelection extends SelectionMethod {
 	public List<Chromosome> select(List<Chromosome> population, int n) {
 		double sum = 0;
 		for(int i = 0; i < population.size(); i++) {
+            Chromosome chr = population.get(i);
+            chr.setFitnessSelection(chr.getFitness());
 			sum += population.get(i).getFitnessSelection();
 		}
 		
