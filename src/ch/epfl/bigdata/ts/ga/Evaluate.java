7c26c12037d915c63837719eefaf79fecf50611b
Filip Hrisafov <filip.hrisafov@gmail.com>
2014-05-09 17:48:42 +0200

    created easy way to benchmark and train

--- src/ch/epfl/bigdata/ts/ga/Evaluate.java
+++ src/ch/epfl/bigdata/ts/ga/Evaluate.java
@@ -9,6 +9,7 @@ import ch.epfl.bigdata.ts.ga.selection.SelectionMethod;
 import ch.epfl.bigdata.ts.ga.util.Util;
 import ch.epfl.bigdata.ts.pattern.fitness.DoubleBottom;
 import ch.epfl.bigdata.ts.pattern.fitness.DoubleTop;
+import ch.epfl.bigdata.ts.pattern.fitness.Rectangle;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,24 +21,19 @@ public class Evaluate {
     public static int NUM_OF_CHROMOSOMES = 50;
 
     public static int DOUBLE_BOT_GENE_BOT1_RANGE = 2;
-    public static double val2 []=new double[4];
+    public static double val2[] = new double[4];
 
     public static void main(String[] args) {
 
 
-        List<Chromosome> chromosomes = new ArrayList<Chromosome>();
-
-//        double [] values = {0.025632261337161677, 0.1900228970807991, 0.46556808380738546, 0.2946468966796436};
-        double [] values = val2;
-        addChromosome(chromosomes, values);
         long startTime = System.currentTimeMillis();
-
-        DoubleBottom doubleBottom = new DoubleBottom(12, 3000, 12, 30);
-//        DoubleTop doubleBottom = new DoubleTop(7, 100, 7, 25);
-
-        doubleBottom.calcFitness(chromosomes.get(0));
-
-        Chromosome best = chromosomes.get(0);
+        //        double [] values = {0.025632261337161677, 0.1900228970807991, 0.46556808380738546, 0.2946468966796436};
+        double[] values = val2;
+        int evalNumOfDays = 12;
+        int evalStartMoney = 3000;
+        int evalGenerationWindow = 12;
+        int evalStartData = 30;
+        Chromosome best = evaluateChromosome(values, evalNumOfDays, evalStartMoney, evalGenerationWindow, evalStartData);
 
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
@@ -45,6 +41,21 @@ public class Evaluate {
         System.out.println("Fitness: " + best.getFitness());
         System.out.println("Number of transactions: " + best.getNumberOfTransactions());
         System.out.println("This took " + duration + " milliseconds");
+    }
+
+    public static Chromosome evaluateChromosome(double[] values, int evalNumOfDays, int evalStartMoney, int evalGenerationWindow, int evalStartData) {
+        List<Chromosome> chromosomes = new ArrayList<Chromosome>();
+
+        addChromosome(chromosomes, values);
+
+        DoubleBottom doubleBottom = new DoubleBottom(evalNumOfDays, evalStartMoney, evalGenerationWindow, evalStartData);
+//        DoubleTop doubleBottom = new DoubleTop(7, 100, 7, 25);
+
+//        Rectangle doubleBottom = new Rectangle(11, 3000, 11, 31);
+
+        doubleBottom.calcFitness(chromosomes.get(0));
+
+        return chromosomes.get(0);
     }
 
     private static void addChromosome(List<Chromosome> chromosomes, double[] values) {
--- src/ch/epfl/bigdata/ts/ga/Training.java
+++ src/ch/epfl/bigdata/ts/ga/Training.java
@@ -13,7 +13,11 @@ import ch.epfl.bigdata.ts.ga.util.Util;
 import ch.epfl.bigdata.ts.pattern.fitness.DoubleBottom;
 import ch.epfl.bigdata.ts.pattern.fitness.DoubleTop;
 import ch.epfl.bigdata.ts.pattern.fitness.FitnessFunctionTest;
+import ch.epfl.bigdata.ts.pattern.fitness.Rectangle;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,59 +28,137 @@ public class Training {
     public static int NUM_OF_CHROMOSOMES = 200;
 
     public static int DOUBLE_BOT_GENE_BOT1_RANGE = 2;
+    public static String fileOut = "doubleBottom.txt";
+    public static int ITERATIONS = 10;
+
 
     public static void main(String[] args) {
 
+        PrintStream out;
+
+        try {
+            out = new PrintStream(new FileOutputStream(fileOut));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        out.println("NUM_OF_CHROMOSOMES = " + NUM_OF_CHROMOSOMES);
+        int numOfDays = 30;
+        int startMoney = 3000;
+        int generationWindow = 5;
+        int startData = 0;
+
+        int evalNumOfDays = 12;
+        int evalStartMoney = 3000;
+        int evalGenerationWindow = 12;
+        int evalStartData = 30;
 
         List<Chromosome> chromosomes = new ArrayList<Chromosome>();
+        List<Chromosome> evalResults = new ArrayList<Chromosome>();
+        List<Chromosome> trainResults = new ArrayList<Chromosome>();
 
         //double [] values = {7, 5, 3, 1};
         //addChromosome(chromosomes, values);
-        long startTime = System.currentTimeMillis();
+        for (int j = 0; j < ITERATIONS; j++) {
 
-        Random random = new Random(System.currentTimeMillis());
+            out.println("ITERATION #" + j);
+            long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < NUM_OF_CHROMOSOMES; i++) {
+            Random random = new Random(System.currentTimeMillis());
+            chromosomes.clear();
 
-            double bot1 = random.nextDouble();
-            double bot2 = random.nextDouble();
-            double protSellGain = 0.1 + (0.5 - 0.1) * random.nextDouble();//range between 10 and 50 %
-            double protSellLoss = 0.1 + (0.3 - 0.1) * random.nextDouble();//range between 10 and 40 %
+            for (int i = 0; i < NUM_OF_CHROMOSOMES; i++) {
+
+                double bot1 = random.nextDouble();
+                double bot2 = random.nextDouble();
+                double protSellGain = 0.1 + (0.5 - 0.1) * random.nextDouble();//range between 10 and 50 %
+                double protSellLoss = 0.1 + (0.3 - 0.1) * random.nextDouble();//range between 10 and 40 %
 
 
-            //double [] values0 = {12 + i, 5 + i, 23 + i, 8 + i};
-            //addChromosome(chromosomes, values0);
+                //double [] values0 = {12 + i, 5 + i, 23 + i, 8 + i};
+                //addChromosome(chromosomes, values0);
 
-            double[] values = {bot1, bot2, protSellGain, protSellLoss};
-            addChromosome(chromosomes, values);
-        }
+                double[] values = {bot1, bot2, protSellGain, protSellLoss};
+                addChromosome(chromosomes, values);
+            }
 
-        HashMap<String, Util.Range> geneRange = new HashMap<String, Util.Range>();
-        geneRange.put("a", new Util.Range(0, 1));
-        geneRange.put("b", new Util.Range(0, 1));
-        geneRange.put("c", new Util.Range(0.1, 0.5));
-        geneRange.put("d", new Util.Range(0.1, 0.3));
+            HashMap<String, Util.Range> geneRange = new HashMap<String, Util.Range>();
+            // DoubleBottom range
+            geneRange.put("a", new Util.Range(0, 1));
+            geneRange.put("b", new Util.Range(0, 1));
+            geneRange.put("c", new Util.Range(0.1, 0.5));
+            geneRange.put("d", new Util.Range(0.1, 0.3));
 
-        SelectionMethod selMethod = new RouletteWheelSelection();//new RouletteWheelSelection(); //new RankSelection();
-        CrossoverMethod crossMethod = new SinglePointCrossover(); //new SinglePointCrossover(); //TwoPointCrossover(); UniformCrossover();
-        MutationMethod mutatMethod = new UniformMutation();
+            //Rectangle range
+//        geneRange.put("a", new Util.Range(0, 5));
+//        geneRange.put("b", new Util.Range(0, 3));
+//        geneRange.put("c", new Util.Range(0.1, 0.5));
+//        geneRange.put("d", new Util.Range(0.1, 0.3));
 
-        Chromosome best = GeneticAlgorithm.run(chromosomes, geneRange, new DoubleBottom(30, 3000, 5, 0), selMethod, crossMethod, mutatMethod);
+
+            SelectionMethod selMethod = new RouletteWheelSelection();//new RouletteWheelSelection(); //new RankSelection();
+            CrossoverMethod crossMethod = new SinglePointCrossover(); //new SinglePointCrossover(); //TwoPointCrossover(); UniformCrossover();
+            MutationMethod mutatMethod = new UniformMutation();
+
+//        Chromosome best = GeneticAlgorithm.run(chromosomes, geneRange, new DoubleBottom(30, 3000, 5, 0), selMethod, crossMethod, mutatMethod);
 //        Chromosome best = GeneticAlgorithm.run(chromosomes, geneRange, new DoubleBottom(15, 100, 5, 0), selMethod, crossMethod, mutatMethod);
 
+
+            out.println("Number of days for training: " + numOfDays + ", starting amount of money: " + startMoney + ", window for training: " + generationWindow + ", startData for trading: " + startData);
+            Chromosome best = GeneticAlgorithm.run(chromosomes, geneRange, new Rectangle(numOfDays, startMoney, generationWindow, startData), selMethod, crossMethod, mutatMethod);
 //        Chromosome best = GeneticAlgorithm.run(chromosomes, geneRange, new FitnessFunctionTest(), selMethod, crossMethod, mutatMethod);
 
-        long endTime = System.currentTimeMillis();
-        long duration = endTime - startTime;
-        System.out.println(best);
-        System.out.println("Fitness: " + best.getFitness());
-        System.out.println("Number of transactions: " + best.getNumberOfTransactions());
-        System.out.println("This took " + duration + " milliseconds");
-        Evaluate.val2[0] = best.getGenes().get(0).getValue();
-        Evaluate.val2[1] = best.getGenes().get(1).getValue();
-        Evaluate.val2[2] = best.getGenes().get(2).getValue();
-        Evaluate.val2[3] = best.getGenes().get(3).getValue();
-        Evaluate.main(null);
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+            trainResults.add(best);
+            out.println(best);
+            out.println("Fitness: " + best.getFitness());
+            out.println("Number of transactions: " + best.getNumberOfTransactions());
+            out.println("This took " + duration + " milliseconds");
+//        System.out.println(best);
+//        System.out.println("Fitness: " + best.getFitness());
+//        System.out.println("Number of transactions: " + best.getNumberOfTransactions());
+//        System.out.println("This took " + duration + " milliseconds");
+
+            double[] evalGenes = new double[4];
+
+            evalGenes[0] = best.getGenes().get(0).getValue();
+            evalGenes[1] = best.getGenes().get(1).getValue();
+            evalGenes[2] = best.getGenes().get(2).getValue();
+            evalGenes[3] = best.getGenes().get(3).getValue();
+
+            startTime = System.currentTimeMillis();
+
+            best = Evaluate.evaluateChromosome(evalGenes, evalNumOfDays, evalStartMoney, evalGenerationWindow, evalStartData);
+
+            endTime = System.currentTimeMillis();
+            duration = endTime - startTime;
+
+            evalResults.add(best);
+            out.println("Evaluation phase");
+            out.println(best);
+            out.println("Fitness: " + best.getFitness());
+            out.println("Number of transactions: " + best.getNumberOfTransactions());
+            out.println("This took " + duration + " milliseconds");
+            out.println("EBD OF ITERATION #" + j);
+            out.println();
+        }
+        double sum = 0;
+
+        for (int i = 0; i < evalResults.size(); i++) {
+            sum += evalResults.get(i).getFitness();
+        }
+
+        out.println("AVERAGE CHROMOSOME FITNESS FOR " + ITERATIONS + " iterations is: " + sum / evalResults.size());
+
+
+//        Evaluate.val2[0] = best.getGenes().get(0).getValue();
+//        Evaluate.val2[1] = best.getGenes().get(1).getValue();
+//        Evaluate.val2[2] = best.getGenes().get(2).getValue();
+//        Evaluate.val2[3] = best.getGenes().get(3).getValue();
+//        Evaluate.main(null);
+
+        out.close();
     }
 
     private static void addChromosome(List<Chromosome> chromosomes, double[] values) {
